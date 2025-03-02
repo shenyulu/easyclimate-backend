8b00753729b6648d2a43e6c0dff1e42e3bc59973
Shenyulu <59901836+shenyulu@users.noreply.github.com>
2025-03-02 09:38:47 +0800

    fix: build docs misleading

--- build_wheel_windows.ps1
+++ build_wheel_windows.ps1
@@ -1,19 +1,3 @@
-# git steidani/ConTrack
-# ------------------------------
-# $currentPath = Get-Location
-# Set-Location $currentPath
-
-# Remove-Item "$currentPath\easyclimate_backend\contrack" -Recurse -Force -ErrorAction SilentlyContinue -ErrorVariable err
-# if ($err) {
-#     Write-Host "Not find contrack folder, continue..."
-# }
-# New-Item -Path "$currentPath\tmp" -ItemType Directory
-# git clone https://github.com/steidani/ConTrack.git "$currentPath\tmp"
-# Copy-Item "$currentPath\tmp\contrack" "$currentPath\easyclimate_backend\contrack" -Recurse -Force
-# Remove-Item "$currentPath\tmp" -Recurse -Force
-
-# build wheel
-# ------------------------------
 $Env:CC="cl"
 $Env:FC="ifx"
 Get-ChildItem -Path . -Recurse -Directory -Filter __pycache__ | Remove-Item -Recurse -Force
