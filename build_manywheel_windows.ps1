443521b3baff0ec5aa912acc3d0ad1a730201709
Shenyulu <59901836+shenyulu@users.noreply.github.com>
2025-03-02 09:34:45 +0800

    feat: add en discription

--- build_manywheel_windows.ps1
+++ build_manywheel_windows.ps1
@@ -1,24 +1,31 @@
+# python interpreter register directory
 # python解释器暂存目录
 $targetPath = (Join-Path $env:TEMP "tmp_ecl")
 
+# Basic parameters
 # 基本参数
 $localpath = (Get-Location).Path
 ${repository_python_build_requirement} = Join-Path $localpath 'build_requirement_windows.txt'
 $deploy_win = 'deploy_win'
 
+# Extract file
 # 解压文件
 Expand-Archive -Path (Join-Path $localpath $deploy_win 'pyenv-win.zip') -DestinationPath $targetPath
 
+# pyenv-win basic parameters
 # pyenv-win基本参数
 $Env:PYENV = Join-Path $targetPath 'pyenv-win' 'pyenv-win'
 $Env:PATH += ";" + (Join-Path $Env:PYENV "bin") + ";" + (Join-Path $Env:PYENV "shims")
+# Set up mirror
 # 设置镜像
 $env:PYTHON_BUILD_MIRROR_URL="https://mirrors.huaweicloud.com/python/"
 pyenv update
 
+# Install python
 # 安装python
 pyenv install 3.10.11 3.11.9 3.12.9 3.13.2
 
+# Install dependencies and create virtual environments
 # 安装依赖，创建虚拟环境
 $pythonPath_310 = Join-Path  $Env:PYENV  "versions" "3.10.11" "python.exe"
 & $pythonPath_310 -m pip install virtualenv
@@ -44,12 +51,14 @@ $targetenvPath = (Join-Path $targetPath venv_py313)
 & $pythonPath_313 -m virtualenv $targetenvPath
 '& ' + (Join-Path $targetenvPath "Scripts" "activate.ps1") | Out-File -FilePath (Join-Path $localpath $deploy_win "activate_py313env.ps1")
 
+# Compile
 # 编译
 & (Join-Path '.\'$deploy_win "activate_py310env.ps1") && & python -m pip install -r ${repository_python_build_requirement} && & .\build_wheel_windows.ps1
 & (Join-Path '.\'$deploy_win "activate_py311env.ps1") && & python -m pip install -r ${repository_python_build_requirement} && & .\build_wheel_windows.ps1
 & (Join-Path '.\'$deploy_win "activate_py312env.ps1") && & python -m pip install -r ${repository_python_build_requirement} && & .\build_wheel_windows.ps1
 & (Join-Path '.\'$deploy_win "activate_py313env.ps1") && & python -m pip install -r ${repository_python_build_requirement} && & .\build_wheel_windows.ps1
 
+# Delete files to Recycle bin
 # 删除文件至回收站
 # https://stackoverflow.com/questions/502002/how-do-i-move-a-file-to-the-recycle-bin-using-powershell
 deactivate
