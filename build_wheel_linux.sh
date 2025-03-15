49f57ae3a80f021aa07e00b4160e2b056cb6f0a4
Shenyulu <59901836+shenyulu@users.noreply.github.com>
2025-03-15 22:13:54 +0800

    fix: link error in gcc

--- build_wheel_linux.sh
+++ build_wheel_linux.sh
@@ -1,4 +1,4 @@
-export CC=gcc
+export CC=icx # Not use `gcc` for link error
 export FC=ifx
 find . -type d -name "__pycache__" -exec rm -rf {} +
 # https://github.com/mesonbuild/meson-python/issues/507
--- easyclimate_backend/__init__.py
+++ easyclimate_backend/__init__.py
@@ -1,2 +1,2 @@
 # Version number
-__version__ = "2025.3.0"
+__version__ = "2025.3.1"
