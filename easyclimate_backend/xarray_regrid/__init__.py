7da753e9852ddb79eaeb6b26f799441d385e87f3
Shenyulu <59901836+shenyulu@users.noreply.github.com>
2026-01-26 10:28:45 +0800

    feat: add xarray-regrid

---
+++ easyclimate_backend/xarray_regrid/__init__.py
@@ -0,0 +1,12 @@
+from xarray_regrid import methods
+from xarray_regrid.regrid import Regridder
+from xarray_regrid.utils import Grid, create_regridding_dataset
+
+__all__ = [
+    "Grid",
+    "Regridder",
+    "create_regridding_dataset",
+    "methods",
+]
+
+__version__ = "0.4.1"
